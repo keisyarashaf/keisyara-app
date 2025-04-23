9c98e45b5f8f87afe522b0df27ae810d9c5444c6
keisyarashaf <keisyarashaf@gmail.com>
2025-04-23 15:28:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("choi san gf :3")
 st.write(
     "༓ 𓂃  “a garden bloomed in her defiance.” kissed by fairy, anointed in moonlight. 🪽 [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("e18c3d400063873ec1c8bba9bb46a206.jpg")

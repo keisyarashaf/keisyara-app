83104dad559e1740ca887ff707aded713ea7e158
keisyarashaf <keisyarashaf@gmail.com>
2025-04-23 14:52:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("straw-ie tinged  𓏔  ✿ ")
+st.title("straw-ie tinged")
 st.write(
     "༓ 𓂃  “a garden bloomed in her defiance.” kissed by fairy, anointed in moonlight. 🪽 [docs.streamlit.io](https://docs.streamlit.io/)."
 )

9833991f1d06689988596a65721b14e63c35ad71
keisyarashaf <keisyarashaf@gmail.com>
2025-05-21 15:11:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ st.write(
     "༓ 𓂃  “a garden bloomed in her defiance.” kissed by fairy, anointed in moonlight. 🪽 [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.image("e18c3d400063873ec1c8bba9bb46a206.jpg")
-st.write("\n")
 
 st.title("Aplikasi Sederhana")
 st.header("Aplikasi Mengecek Nilai Genap atau Ganjil")
